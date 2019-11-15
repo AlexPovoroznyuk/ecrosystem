5400d520e94678d4c99d7d4b67bd1ea0d391c740
Alex <lexesss64@gmail.com>
2019-11-15 13:18:12 +0200

    comment parralax

--- src/js/custom/main.js
+++ src/js/custom/main.js
@@ -4,40 +4,40 @@ $(".timeline").slick({
 })
 
 
-function Paralax(parent, element,transition, animationSpeed, offset, adaptive){
-	var adaptiveMod;
-	if(!adaptive) {
-		adaptiveMod = 992;
-	}
-	if(adaptive) {
-		adaptiveMod = adaptive;
-	}
-	if(parent !== null && window.innerWidth > adaptiveMod){
-		var sizeCalc = parseInt(getComputedStyle(document.body).fontSize) / 20;
-		var elemOffset = parent.clientHeight / (animationSpeed * sizeCalc);
-		if(window.pageYOffset + parent.clientHeight > parent.offsetTop){
-			var scrollSize = window.pageYOffset;
-			if(offset){
+// function Paralax(parent, element,transition, animationSpeed, offset, adaptive){
+// 	var adaptiveMod;
+// 	if(!adaptive) {
+// 		adaptiveMod = 992;
+// 	}
+// 	if(adaptive) {
+// 		adaptiveMod = adaptive;
+// 	}
+// 	if(parent !== null && window.innerWidth > adaptiveMod){
+// 		var sizeCalc = parseInt(getComputedStyle(document.body).fontSize) / 20;
+// 		var elemOffset = parent.clientHeight / (animationSpeed * sizeCalc);
+// 		if(window.pageYOffset + parent.clientHeight > parent.offsetTop){
+// 			var scrollSize = window.pageYOffset;
+// 			if(offset){
 				
-				element.setAttribute("style", "top:" + ((parent.offsetTop + parent.clientHeight - window.innerHeight) / (animationSpeed * sizeCalc)) + "px;");
-			}
-			TweenMax.to(element, transition, {y: -scrollSize / (animationSpeed * sizeCalc)});
-		}
-	}
-	if(!(window.innerWidth > adaptiveMod)){
-		element.setAttribute("style", "top:" + "" + "transform:" + "");
-		element.css("transform", "");
-	}
-}
+// 				element.setAttribute("style", "top:" + ((parent.offsetTop + parent.clientHeight - window.innerHeight) / (animationSpeed * sizeCalc)) + "px;");
+// 			}
+// 			TweenMax.to(element, transition, {y: -scrollSize / (animationSpeed * sizeCalc)});
+// 		}
+// 	}
+// 	if(!(window.innerWidth > adaptiveMod)){
+// 		element.setAttribute("style", "top:" + "" + "transform:" + "");
+// 		element.css("transform", "");
+// 	}
+// }
 
-document.querySelectorAll('.parralax-element').forEach(function(element) {
-    var ParalaxParent = element.closest('.parralax-parent');
-    var ParalaxElement = element;
-    var speedAnim = element.getAttribute('data-speed')
-    document.addEventListener('scroll', function (e) {
-        Paralax(ParalaxParent, ParalaxElement, 0.1, speedAnim, true,1);
-    });
-})
+// document.querySelectorAll('.parralax-element').forEach(function(element) {
+//     var ParalaxParent = element.closest('.parralax-parent');
+//     var ParalaxElement = element;
+//     var speedAnim = element.getAttribute('data-speed')
+//     document.addEventListener('scroll', function (e) {
+//         Paralax(ParalaxParent, ParalaxElement, 0.1, speedAnim, true,1);
+//     });
+// })
 
 document.addEventListener('scroll', function (e) {
 	var currentSection;
